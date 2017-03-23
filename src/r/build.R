57a8e41b7184cf561044cbb4823cab98da652fbb
csiu <celia.sy.siu@gmail.com>
2017-03-22 17:31:27 -0700

    Include in build

--- src/r/build.R
+++ src/r/build.R
@@ -3,4 +3,5 @@ rmarkdown::render_site(file.path(PROJHOME, "src/rmd"))
 
 ## Generate specific pages
 rmarkdown::render_site(file.path(PROJHOME, "src/rmd/samples-h3k4me3.Rmd"))
+rmarkdown::render_site(file.path(PROJHOME, "src/rmd/features.Rmd"))
 
